2b23ab2af2744fedc8134e5518562f1a1938d3e7
LEWIS GUEI <lewisguei67@gmail.com>
2025-04-16 22:23:43 +0000

    mise a jours a 22h 23

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -24,11 +24,11 @@ const Home = ({textmode}:TextMode) => {
         <>
          <div className=' w-7/12  '>
          <div className=' size-16 rounded-full'>
-                <img src={`src/images/profile.jpeg`} alt="photo de profile" 
+                <img src={`images/profile.jpeg`} alt="photo de profile" 
                 className='  size-full object-center object-cover rounded-full'
                 />
          </div>
-         <h1 className= {`py-10 text-5xl  font-bold ${ textmode ?  `text-gray-900`: `text-white`} `} >
+         <h1 className= {`py-10 text-5xl  font-bold ${ textmode ?  `text-gray-900`:`text-white`} `} >
         Bonjour je suis Guei Lewis, mais
          vous pouvez m'appeller Wisley
          </h1>
@@ -39,12 +39,12 @@ const Home = ({textmode}:TextMode) => {
          développe lndev-ui et d'autres projets intéressants . Je suis passionné par la création de sites
          </p>
          <p className=' text-gray-600'>
-         web esthétiques, intuitifs et réactifs.
+         web esthétiques, intuitifs et réactifs.   
         
          </p>
 
-          <ul className=' flex  w-2/5 my-10 justify-around text-gray-500'>
-        <BsTwitterX className=' text-xl' />
+          <ul className=' flex w-2/5 my-10 justify-around text-gray-500'>
+        <BsTwitterX className=' text-xl'/>
         <FaGithub className=' text-xl' />
         <FaLinkedin className=' text-xl' />
         <FaThreads className=' text-xl' />
@@ -58,16 +58,16 @@ const Home = ({textmode}:TextMode) => {
   <div className=' border p-1 rounded-3xl'>
     <div className=' border p-10 rounded-3xl'>
     <div className=' flex items-center '>
-      <div className=' mr-2 size-12 p-2 bg-gray-200 rounded-full'>     <img src={`src/images/lemon.svg`} alt="" className='object-cover object-center size-full rounded-full' /></div>
+      <div className=' mr-2 size-12 p-2 bg-gray-200 rounded-full'>     <img src={`images/lemon.svg`} alt="" className='object-cover object-center size-full rounded-full' /></div>
 <h1 className='text-sm font-semibold'>Indev/ui</h1>
     </div>
     <p className='py-5 '>Une collection amusante de petits composants bien codés pour rationaliser votre processus de développement.</p>
   <div className=' w-48 flex '>
  <div className=' border border-white rounded-full bg-gray-200 relative'> <img src={`src/images/memo_1.png`} alt="" /></div>
- <div className=' border border-white rounded-full bg-gray-200 relative right-2 '> <img src={`src/images/memo_2.png`} alt="" /></div>
- <div className=' border border-white rounded-full bg-gray-200 relative right-4 '> <img src={`src/images/memo_3.png`} alt="" /></div>
- <div className=' border border-white rounded-full bg-gray-200 relative right-6 '> <img src={`src/images/memo_4.png`} alt="" /></div>
- <div className=' border border-white rounded-full bg-gray-200 relative right-8 '> <img src={`src/images/memo_5.png`} alt="" /></div>
+ <div className=' border border-white rounded-full bg-gray-200 relative right-2 '> <img src={`images/memo_2.png`} alt="" /></div>
+ <div className=' border border-white rounded-full bg-gray-200 relative right-4 '> <img src={`images/memo_3.png`} alt="" /></div>
+ <div className=' border border-white rounded-full bg-gray-200 relative right-6 '> <img src={`images/memo_4.png`} alt="" /></div>
+ <div className=' border border-white rounded-full bg-gray-200 relative right-8 '> <img src={`images/memo_5.png`} alt="" /></div>
    </div>
   
   
